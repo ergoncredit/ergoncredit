39602b897c6f07705c3649cd5b7820cc28633552
ergoncredit <107065076+ergoncredit@users.noreply.github.com>
2022-06-07 14:07:47 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,11 +8,11 @@ setup(
   author = 'Pedro Borges',                   # Type in your name
   author_email = 'pedro.borges@ergoncredit.com.br',      # Type in your E-Mail
   url = 'https://github.com/ergoncredit/ergoncredit',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/ergoncredit/ergoncredit/archive/v_01.tar.gz',    # I explain this later on
   keywords = ['Backoffice', 'Api', 'Ergoncredit'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
-          'validators',
-          'beautifulsoup4',
+          'requests',
+          'browser_cookie3',
       ],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
