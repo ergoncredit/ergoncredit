970ca81627cdf448066f76a8f747087d913c2c5d
ergoncredit <107065076+ergoncredit@users.noreply.github.com>
2022-06-08 15:24:57 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'ergoncredit',         # How you named your package folder (MyLib)
   packages = ['ergoncredit'],   # Chose the same as "name"
-  version = '1.0',      # Start with a small number and increase it with every change you make
+  version = '1.2',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'A simple module to return json for ergoncredit environment.',   # Give a short description about your library
   author = 'Pedro Borges',                   # Type in your name
   author_email = 'pedro.borges@ergoncredit.com.br',      # Type in your E-Mail
   url = 'https://github.com/ergoncredit/ergoncredit',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/ergoncredit/ergoncredit/archive/refs/tags/1.02.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/ergoncredit/ergoncredit/archive/refs/tags/1.2.tar.gz',    # I explain this later on
   keywords = ['Backoffice', 'Api', 'Ergoncredit'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'requests',
